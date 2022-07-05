498ae74cbb1f6effe0189a7efc1abc2cf6249b7c
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-05 17:22:55 -0500

    serialization of a user object

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const passport = require('passport');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title:'Hello',message:"Please login"});
