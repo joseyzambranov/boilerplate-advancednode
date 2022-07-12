d355a3db2a2c80aea1512e04e64bbb5278d3cd21
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-11 19:09:24 -0500

    clean proyect with modules fcc example

--- server.js
+++ server.js
@@ -1,42 +1,54 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const routes = require('./routes');
-const auth = require('./auth.js');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const PORT = process.env.PORT || 3000;
+const router = require("./routes");
+const auth = require("./auth");
+
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
-fccTesting(app); // For fCC testing purposes
+fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false },
+  cookie: {
+    secure: false
+  }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 myDB(async (client) => {
+
   const myDataBase = await client.db('database').collection('users');
 
-  routes(app, myDataBase);
-  auth(app, myDataBase);
+  router(app, myDataBase)
+
+  auth(app, myDataBase)
+
 }).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('pug', { title: e, message: 'Unable to login' });
-  });
-});
+    res.render('pug', {
+      title: e,
+      message: "Unable login"})
+  })
+})
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
 });
