fb6db8c6458957e5164659e91d865bdd23179ad1
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-15 17:23:14 -0500

    comunicate by emitting

--- server.js
+++ server.js
@@ -38,8 +38,13 @@ myDB(async (client) => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let currentUsers = 0;
+
   io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user count', currentUsers);
     console.log('A user has connected');
+    
   });
 
 }).catch((e) => {
